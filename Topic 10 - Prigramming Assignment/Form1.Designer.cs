87a0de016630b45d3a2cd8d2a5f88a04c5b06ce6
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-23 09:08:49 -0400

    Add project files.

---
+++ Topic 10 - Prigramming Assignment/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Topic_10___Prigramming_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnBread = new System.Windows.Forms.Button();
+            this.btnChicken = new System.Windows.Forms.Button();
+            this.btnCurry = new System.Windows.Forms.Button();
+            this.btnCornbread = new System.Windows.Forms.Button();
+            this.btnRoti = new System.Windows.Forms.Button();
+            this.lblCornbreadDisc = new System.Windows.Forms.Label();
+            this.lblWhitebreadDisc = new System.Windows.Forms.Label();
+            this.lblChickenDisc = new System.Windows.Forms.Label();
+            this.lblCurrygoatDisc = new System.Windows.Forms.Label();
+            this.lblRotiDisc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(200, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(196, 26);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(337, 20);
+            this.lblInstructions.TabIndex = 6;
+            this.lblInstructions.Text = "Click the options on the left to get info on them";
+            // 
+            // btnBread
+            // 
+            this.btnBread.Location = new System.Drawing.Point(29, 93);
+            this.btnBread.Name = "btnBread";
+            this.btnBread.Size = new System.Drawing.Size(94, 23);
+            this.btnBread.TabIndex = 7;
+            this.btnBread.Text = "White Bread";
+            this.btnBread.UseVisualStyleBackColor = true;
+            this.btnBread.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnChicken
+            // 
+            this.btnChicken.Location = new System.Drawing.Point(29, 143);
+            this.btnChicken.Name = "btnChicken";
+            this.btnChicken.Size = new System.Drawing.Size(94, 23);
+            this.btnChicken.TabIndex = 8;
+            this.btnChicken.Text = "Fired Chicken";
+            this.btnChicken.UseVisualStyleBackColor = true;
+            this.btnChicken.Click += new System.EventHandler(this.btnChicken_Click);
+            // 
+            // btnCurry
+            // 
+            this.btnCurry.Location = new System.Drawing.Point(29, 189);
+            this.btnCurry.Name = "btnCurry";
+            this.btnCurry.Size = new System.Drawing.Size(94, 23);
+            this.btnCurry.TabIndex = 9;
+            this.btnCurry.Text = "Curry Goat";
+            this.btnCurry.UseVisualStyleBackColor = true;
+            this.btnCurry.Click += new System.EventHandler(this.btnCurry_Click);
+            // 
+            // btnCornbread
+            // 
+            this.btnCornbread.Location = new System.Drawing.Point(29, 239);
+            this.btnCornbread.Name = "btnCornbread";
+            this.btnCornbread.Size = new System.Drawing.Size(94, 23);
+            this.btnCornbread.TabIndex = 10;
+            this.btnCornbread.Text = "Cornbread";
+            this.btnCornbread.UseVisualStyleBackColor = true;
+            this.btnCornbread.Click += new System.EventHandler(this.btnCornbread_Click);
+            // 
+            // btnRoti
+            // 
+            this.btnRoti.Location = new System.Drawing.Point(29, 289);
+            this.btnRoti.Name = "btnRoti";
+            this.btnRoti.Size = new System.Drawing.Size(94, 23);
+            this.btnRoti.TabIndex = 11;
+            this.btnRoti.Text = "Roti";
+            this.btnRoti.UseVisualStyleBackColor = true;
+            this.btnRoti.Click += new System.EventHandler(this.btnRoti_Click);
+            // 
+            // lblCornbreadDisc
+            // 
+            this.lblCornbreadDisc.AutoSize = true;
+            this.lblCornbreadDisc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCornbreadDisc.Location = new System.Drawing.Point(235, 325);
+            this.lblCornbreadDisc.Name = "lblCornbreadDisc";
+            this.lblCornbreadDisc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCornbreadDisc.Size = new System.Drawing.Size(279, 26);
+            this.lblCornbreadDisc.TabIndex = 12;
+            this.lblCornbreadDisc.Text = "Cornbread was originaly made in Mesoamerican,\r\n Native American, and African coun" +
+    "trys";
+            this.lblCornbreadDisc.Visible = false;
+            // 
+            // lblWhitebreadDisc
+            // 
+            this.lblWhitebreadDisc.AutoSize = true;
+            this.lblWhitebreadDisc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWhitebreadDisc.Location = new System.Drawing.Point(225, 325);
+            this.lblWhitebreadDisc.Name = "lblWhitebreadDisc";
+            this.lblWhitebreadDisc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblWhitebreadDisc.Size = new System.Drawing.Size(289, 39);
+            this.lblWhitebreadDisc.TabIndex = 13;
+            this.lblWhitebreadDisc.Text = "The invention of white bread can be traced back \r\nto some of the oldest civilisat" +
+    "ions, such as\r\n Mesopotamia as well as the settlements in Egypt.";
+            this.lblWhitebreadDisc.Visible = false;
+            // 
+            // lblChickenDisc
+            // 
+            this.lblChickenDisc.AutoSize = true;
+            this.lblChickenDisc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChickenDisc.Location = new System.Drawing.Point(198, 325);
+            this.lblChickenDisc.Name = "lblChickenDisc";
+            this.lblChickenDisc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblChickenDisc.Size = new System.Drawing.Size(335, 26);
+            this.lblChickenDisc.TabIndex = 14;
+            this.lblChickenDisc.Text = "Pan frying, deep frying and roasting chicken are the three\r\n main ways to make th" +
+    "is world-famous dish.";
+            this.lblChickenDisc.Visible = false;
+            // 
+            // lblCurrygoatDisc
+            // 
+            this.lblCurrygoatDisc.AutoSize = true;
+            this.lblCurrygoatDisc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrygoatDisc.Location = new System.Drawing.Point(235, 325);
+            this.lblCurrygoatDisc.Name = "lblCurrygoatDisc";
+            this.lblCurrygoatDisc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCurrygoatDisc.Size = new System.Drawing.Size(265, 26);
+            this.lblCurrygoatDisc.TabIndex = 15;
+            this.lblCurrygoatDisc.Text = "In the 17 century curry and other spices were\r\n brought to Jamaica by Eastern Ind" +
+    "ians.";
+            this.lblCurrygoatDisc.Visible = false;
+            // 
+            // lblRotiDisc
+            // 
+            this.lblRotiDisc.AutoSize = true;
+            this.lblRotiDisc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRotiDisc.Location = new System.Drawing.Point(235, 325);
+            this.lblRotiDisc.Name = "lblRotiDisc";
+            this.lblRotiDisc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblRotiDisc.Size = new System.Drawing.Size(290, 26);
+            this.lblRotiDisc.TabIndex = 16;
+            this.lblRotiDisc.Text = "Roti is unleavened flatbread made in kitchens\r\n throughout South Asia, the Caribb" +
+    "ean and Africa.";
+            this.lblRotiDisc.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(619, 444);
+            this.Controls.Add(this.lblRotiDisc);
+            this.Controls.Add(this.lblCurrygoatDisc);
+            this.Controls.Add(this.lblChickenDisc);
+            this.Controls.Add(this.lblWhitebreadDisc);
+            this.Controls.Add(this.lblCornbreadDisc);
+            this.Controls.Add(this.btnRoti);
+            this.Controls.Add(this.btnCornbread);
+            this.Controls.Add(this.btnCurry);
+            this.Controls.Add(this.btnChicken);
+            this.Controls.Add(this.btnBread);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnBread;
+        private System.Windows.Forms.Button btnChicken;
+        private System.Windows.Forms.Button btnCurry;
+        private System.Windows.Forms.Button btnCornbread;
+        private System.Windows.Forms.Button btnRoti;
+        private System.Windows.Forms.Label lblCornbreadDisc;
+        private System.Windows.Forms.Label lblWhitebreadDisc;
+        private System.Windows.Forms.Label lblChickenDisc;
+        private System.Windows.Forms.Label lblCurrygoatDisc;
+        private System.Windows.Forms.Label lblRotiDisc;
+    }
+}
+
---
+++ Topic 10 - Prigramming Assignment/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_10___Prigramming_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.WhiteBread;
+            lblWhitebreadDisc.Visible = true;
+
+            lblChickenDisc.Visible = false;
+            lblCornbreadDisc.Visible = false;
+            lblCurrygoatDisc.Visible = false;
+            lblRotiDisc.Visible = false;
+        }
+
+        private void btnChicken_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Fried_Chicken_Pieces_1;
+            lblChickenDisc.Visible = true;
+
+            lblCornbreadDisc.Visible = false;
+            lblCurrygoatDisc.Visible = false;
+            lblWhitebreadDisc.Visible = false;
+            lblRotiDisc.Visible = false;
+        }
+
+        private void btnCurry_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.CurryGoat;
+            lblCurrygoatDisc.Visible = true;
+
+            lblChickenDisc.Visible = false;
+            lblCornbreadDisc.Visible = false;
+            lblWhitebreadDisc.Visible = false;
+            lblRotiDisc.Visible = false;
+        }
+
+        private void btnCornbread_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Cornbread;
+            lblCornbreadDisc.Visible = true;
+
+            lblChickenDisc.Visible = false;
+            lblCurrygoatDisc.Visible = false;
+            lblWhitebreadDisc.Visible = false;
+            lblRotiDisc.Visible = false;
+        }
+
+        private void btnRoti_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Roti;
+            lblRotiDisc.Visible = true;
+
+            lblChickenDisc.Visible = false;
+            lblCornbreadDisc.Visible = false;
+            lblCurrygoatDisc.Visible = false;
+            lblWhitebreadDisc.Visible = false;
+        }
+    }
+}
